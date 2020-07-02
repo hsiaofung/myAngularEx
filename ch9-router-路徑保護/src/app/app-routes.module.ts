0a6077d3fdde65676369ad96bf8fa8914508da0f
Tim Chou - TW (SW) <TimChou@supermicro.com>
2020-07-02 14:44:33 +0800

    add 逗號 in app-routes.module.ts

--- ch9-router-路徑保護/src/app/app-routes.module.ts
+++ ch9-router-路徑保護/src/app/app-routes.module.ts
@@ -8,14 +8,26 @@ import { RegisterComponent } from './user/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'} // 加入預設路徑以重新導向Login頁，full表示完全相符
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // 加入預設路徑以重新導向Login頁，full表示完全相符
   // 宣告應用程式的路徑陣列
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'stocks/list', component: StockListComponent, canActivate: [AuthGuard] },
-  { path: 'stocks/create', component: CreateStockComponent, canActivate: [AuthGuard] },
-  { path: 'stock/:code', component: StockDetailsComponent, canActivate: [AuthGuard] }, // 載入股票細節
-  { path: '**', redirectTo: '/register' } // 捕抓全部路徑，和**比較。
+  {
+    path: 'stocks/list',
+    component: StockListComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'stocks/create',
+    component: CreateStockComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'stock/:code',
+    component: StockDetailsComponent,
+    canActivate: [AuthGuard],
+  }, // 載入股票細節
+  { path: '**', redirectTo: '/register' }, // 捕抓全部路徑，和**比較。
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)], // 匯入並登記根應用程式的路徑
