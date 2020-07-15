063e622fef9b892c127bde8fa55dc9c2c0aff2bc
Tim Chou - TW (SW) <TimChou@supermicro.com>
2020-07-15 16:45:56 +0800

    add redux

---
+++ ch12-ngrx-simple-counter/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './counter.reducer';
+
+@NgModule({
+  declarations: [AppComponent, MyCounterComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
